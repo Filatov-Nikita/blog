9dfd0500794c461a46c173356039be915d5de024
nikita <nelit_040498@mail.ru>
2017-09-29 19:08:46 +0500

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,6 +65,7 @@
         </style>
     </head>
     <body>
+	<?='123'?>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
