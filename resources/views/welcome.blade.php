37f007410567f1789579da5416f370fb3e79c7b3
nikita <nelit_040498@mail.ru>
2017-10-06 03:07:13 +0500

    Test1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,7 +65,7 @@
         </style>
     </head>
     <body>
-	<h1>Test</h1>
+	<h1>Test1/h1>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
